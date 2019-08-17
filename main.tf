8051b06f88792f18ab1492d8e2d954e2e4362573
Sivaramakrishna Reddy Medapati <sivaram.medapati@gmail.com>
2019-08-17 08:22:33 +0000

    updated main.tf

--- main.tf
+++ main.tf
@@ -96,6 +96,28 @@ resource "aws_security_group" "jump_server_sg" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
 }
+resource "aws_security_group" "elb_sg" {
+  name        = "ELB SG"
+  vpc_id      = "${aws_vpc.main.id}"
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
 resource "aws_security_group" "web_server_sg" {
   name        = "Web Server SG"
   vpc_id      = "${aws_vpc.main.id}"
@@ -188,6 +210,7 @@ resource "aws_instance" "jump_instance" {
 resource "aws_instance" "web_instance" {
   count         = "${var.web_instance_count}"
   ami           = "ami-02e60be79e78fef21"
+  associate_public_ip_address = "true"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.web_server_sg.id}"]
   key_name = "clikey"
@@ -207,5 +230,32 @@ resource "aws_instance" "db_instance" {
   subnet_id = "${aws_subnet.prisub.id}"
   tags = {
     Name = "DB Server ${count.index + 1}"
+  }
+}
+resource "aws_elb" "web_elb" {
+  name = "WebServerELB"
+  subnets = ["${aws_subnet.pubsub.id}"]
+  security_groups = ["${aws_security_group.elb_sg.id}"]
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+  instances                   = ["${aws_instance.web_instance[0].id}","${aws_instance.web_instance[1].id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "Web Server ELB"
   }
 }
